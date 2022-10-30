5510dcad579fa008a3ce20504fd916fab4bf2de6
Aaron Molstad <amolstad@fredhutch.org>
2022-10-30 17:05:35 -0400

    Update HierMultinomNestedOverlap.R

--- Application/HierMultinomNestedOverlap.R
+++ Application/HierMultinomNestedOverlap.R
@@ -2,16 +2,10 @@ library(MASS)
 library(Rcpp)
 sourceCpp("scripts/matMult.cpp")
 
-# getGrad_Lik <- function(X, Y, beta){
-#   t0 <- exp(crossprod(t(X), beta))
-#   P <- t0/tcrossprod(colSums(t(t0)), rep(1, dim(beta)[2]))
-#   grad <- crossprod(X, P - Y)/dim(Y)[1]
-#   return(list("grad" = grad, "lik" = -sum(log(rowSums(Y*P)))/dim(Y)[1]))
-# }
 
 getGrad_Lik <- function(X, Y, beta){
   t0 <- eigenMatMult(X, beta)
-  if(any(t0 > 700)){ #710 is exact threshold for Inf
+  if (any(t0 > 700)) { #710 is exact threshold for Inf
     t1 <- exp(t0 - apply(t0, 1, max))
     P <- t1/eigenMatMult(armaRowSums(t1), t(rep(1, dim(beta)[2])))
   } else {
@@ -25,7 +19,7 @@ getGrad_Lik <- function(X, Y, beta){
 evalPen <- function(beta, lambda, gamma, groups){
   p1 <- gamma*sum(apply(beta[-1,], 1, function(x){sqrt(sum(x^2))}))
   p2 <- 0
-  for(kk in 1:length(groups)){
+  for (kk in 1:length(groups)) {
     p2 <- p2 + sum(apply(beta[-1,groups[[kk]]] - rowMeans(beta[-1,groups[[kk]], drop=FALSE])%*%t(rep(1, length(groups[[kk]]))), 1, function(x){sqrt(sum(x^2))}))
   }
   return(p1 + lambda*p2)
@@ -34,7 +28,7 @@ evalPen <- function(beta, lambda, gamma, groups){
 
 getObj <- function(X, Y, beta, groups, lambda, gamma){
   t0 <- eigenMatMult(X, beta)
-  if(any(t0 > 700)){ #710 is exact threshold for Inf
+  if (any(t0 > 700)) { #710 is exact threshold for Inf
     t1 <- exp(t0 - apply(t0, 1, max))
     P <- t1/eigenMatMult(armaRowSums(t1), t(rep(1, dim(beta)[2])))
     t1 <- NULL
@@ -42,12 +36,12 @@ getObj <- function(X, Y, beta, groups, lambda, gamma){
     P <- exp(t0)/eigenMatMult(armaRowSums(exp(t0)), t(rep(1, dim(beta)[2])))
   }
   t0 <- -sum(log(armaRowSums(Y*P)))/dim(Y)[1]
-  if(lambda == 0 & gamma == 0){
+  if (lambda == 0 & gamma == 0) {
     return(t0)
   } else {
     p1 <- gamma*sum(apply(beta[-1,], 1, function(x){sqrt(sum(x^2))}))
     p2 <- 0
-    for(kk in 1:length(groups)){
+    for (kk in 1:length(groups)) {
       p2 <- p2 + sum(apply(beta[-1,groups[[kk]]] - rowMeans(beta[-1,groups[[kk]], drop=FALSE])%*%t(rep(1, length(groups[[kk]]))), 1, function(x){sqrt(sum(x^2))}))
     }
     return(t0 + p1 + lambda*p2)
@@ -55,74 +49,46 @@ getObj <- function(X, Y, beta, groups, lambda, gamma){
 }
 
 
-# Psi  <- function(eta, w, group) {
-  
-#   y <- eta[group]
-#   s <- length(y)
-#   tildey <- y - mean(y)
-#   tildeW <- diag(1, s) - matrix(1, s, s)/s
-#   v <- crossprod(svd(tildeW)$u, tildey)
-#   t1 <- crossprod(tildeW, tildey)
-#   t0 <- sum(t1^2)
-#   if (t0 <= w^2) {
-#     result <- mean(y)*rep(1, s)
-#   } else {
-#     theta <- solve(crossprod(tildeW) + diag(w/(sqrt(sum(v[-s]^2)) - w), s), t1)
-#     result <- theta + mean(y - theta)
-#   }
-#   return(result)
-# }
-
-
 prox.function <- function(eta, lambda, groups, D.input, vmat = NULL){
 
-  if(is.null(vmat)){
+  if (is.null(vmat)) {
     vmat <- matrix(0, length(eta), length(groups))
   }
   D.mats <- list()
-  for(kk in 1:length(groups)){
+  for (kk in 1:length(groups)) {
     s <- length(groups[[kk]])
     D.mats[[kk]] <- diag(1, s) - matrix(1, s, s)/s
   }
 
   y.resid <- eta
-  for(kk in length(groups):1){
+  for (kk in length(groups):1) {
     y.resid[groups[[kk]]] <- y.resid[groups[[kk]]] - crossprod(D.mats[[kk]], vmat[groups[[kk]], kk])
   }
 
-  for(jj in 1:1e4){
+  for (jj in 1:1e4) {
     
     vmatold <- vmat
-    for(kk in length(groups):1){
+    for (kk in length(groups):1) {
       y.resid[groups[[kk]]] <- y.resid[groups[[kk]]] + crossprod(D.mats[[kk]], vmat[groups[[kk]], kk])
       vmat[groups[[kk]],kk] <- crossprod(ginv(D.mats[[kk]]), crossprod(D.mats[[kk]], y.resid[groups[[kk]]]))
-      if(sum(vmat[groups[[kk]],kk]^2) < lambda^2){
+      if (sum(vmat[groups[[kk]],kk]^2) < lambda^2) {
         y.resid[groups[[kk]]] <- y.resid[groups[[kk]]] - crossprod(D.mats[[kk]],vmat[groups[[kk]], kk])
-        #cat("passed", "\n")
       } else {
         eo <- eigen(D.mats[[kk]])
         w <- crossprod(y.resid[groups[[kk]]], eo$vec[,which(abs(eo$values) > 1e-8)])
         t0 <- sum(w^2)
         tau <- sqrt(t0/lambda^2) - 1 
-        #cat("# ------------------", "\n")
-        #tryFunc <- function(x){c(crossprod(solve(crossprod(D.mats[[kk]]) + diag(x, dim(D.mats[[kk]])[1]), crossprod(D.mats[[kk]], y.resid[groups[[kk]]])))) - lambda^2}
-        #out <- uniroot(tryFunc, c(1e-12,1e12), tol=1e-8)
-        #cat(tau, out$root, "\n")
-        # Replace with exact solution 
         vmat[groups[[kk]],kk] <- crossprod(t(ginv(crossprod(D.mats[[kk]]) + diag(tau, dim(D.mats[[kk]])[1]))), crossprod(D.mats[[kk]], y.resid[groups[[kk]]]))
         y.resid[groups[[kk]]] <- y.resid[groups[[kk]]] - crossprod(D.mats[[kk]], vmat[groups[[kk]], kk])
       }
     }
-    #if(jj > 2){
-      if(sum((vmatold - vmat)^2) < 1e-9){
-        #cat("Broken at iteration ", jj,":" "\n")
-        break
-      }
-    #}
+    if (sum((vmatold - vmat)^2) < 1e-9) {
+      break
+    }
   }
 
   t0 <- rep(0, length(eta))
-  for(kk in length(groups):1){
+  for (kk in length(groups):1) {
     t0[groups[[kk]]] <- t0[groups[[kk]]] + crossprod(D.mats[[kk]], vmat[groups[[kk]],kk])
   }
   return(list("result" = eta - t0, "vmat" = vmat))
@@ -137,7 +103,7 @@ prox.function <- function(eta, lambda, groups, D.input, vmat = NULL){
 outer_prox <- function(y, lambda, gamma, groups, vmat){
   
   b.out <- rep(0, length(y))
-  if(lambda!=0){
+  if (lambda!=0) {
     t0 <- prox.function(y, lambda, groups, vmat)
     b.out <- t0$result
     vmat <- t0$vmat
@@ -145,7 +111,7 @@ outer_prox <- function(y, lambda, gamma, groups, vmat){
     b.out <- y
   }
   t0 <- sqrt(sum(b.out^2))
-  if(t0 > gamma){
+  if (t0 > gamma) {
     b.out <- (1 - gamma/t0)*b.out
   } else {
     b.out <- rep(0, length(y))
@@ -156,12 +122,12 @@ outer_prox <- function(y, lambda, gamma, groups, vmat){
 full_prox <- function(input, lambda, gamma, groups, vmat.full = NULL){
   
   out <- matrix(0, nrow=dim(input)[1], ncol=dim(input)[2])
-  if(is.null(vmat.full)){
+  if (is.null(vmat.full)) {
    vmat.full <- array(0, dim=c(dim(input)[1], dim(input)[2], length(groups))) 
   }
   out[1,] <- input[1,]
-  for(j in 2:(dim(input)[1])){
-    if(sqrt(sum(input[j,]^2)) < gamma){
+  for (j in 2:(dim(input)[1])) {
+    if (sqrt(sum(input[j,]^2)) < gamma) {
       out[j,] <- 0
     } else {
       t0 <- outer_prox(input[j,], lambda, gamma, groups, vmat.full[j,,])
@@ -175,7 +141,7 @@ full_prox <- function(input, lambda, gamma, groups, vmat.full = NULL){
 validationLik <- function(X, Xval, Yval, beta){
   x.sd.temp <- apply(X, 2, sd)
   Xval.temp <- cbind(1, (Xval - tcrossprod(rep(1, dim(Xval)[1]), colMeans(X)))/tcrossprod(rep(1, dim(Xval)[1]), x.sd.temp))
-  if(any(x.sd.temp==0)){
+  if (any(x.sd.temp==0)) {
     Xval.temp[,(which(x.sd.temp==0) + 1)] <- 0
   }
   lleval <- crossprod(t(Xval.temp),beta)
@@ -207,7 +173,7 @@ AccPGD_HierMultNestedOverlap <- function(X, Y, groups, beta.init, lambda, gamma,
   L0 <- dim(Y)[1]/(sqrt(dim(Y)[2])*sum(X^2))
   vmat.full <- NULL
   
-  for(kk in 1:max.iter){
+  for (kk in 1:max.iter) {
     
     search.point <- beta.iter + ((alpha.prev - 1)/(alpha))*(beta.iter - beta.prev)
     temp <- getGrad_Lik(X, Y, search.point)
@@ -215,7 +181,7 @@ AccPGD_HierMultNestedOverlap <- function(X, Y, groups, beta.init, lambda, gamma,
     lik.current <- temp$lik
     linesearch <- TRUE
     
-    while(linesearch){
+    while (linesearch) {
       
       temp.step <- search.point - step.size*temp.grad
       t0 <- full_prox(temp.step, step.size*lambda, step.size*gamma, groups, vmat.full = vmat.full)
@@ -224,31 +190,27 @@ AccPGD_HierMultNestedOverlap <- function(X, Y, groups, beta.init, lambda, gamma,
       lik.try <- getObj(X, Y, beta.try, groups, lambda = 0, gamma = 0)
       check <- (lik.try <= lik.current + sum(temp.grad*(beta.try - search.point)) + (1/(2*step.size))*sum((beta.try - search.point)^2))
 
-      if(check | step.size == L0){
+      if (check | step.size == L0) {
         linesearch <- FALSE
         beta.prev <- beta.iter
         beta.iter <- beta.try
         alpha.prev <- alpha
         alpha <- (1 + sqrt(1 + 4*alpha.prev^2))/2
-        #lik.current <- lik.try
       } else {
-        #cat(check, "\n")
         step.size <- max(step.size/2, L0)
       }
     }
     
-    # cat(step.size, "\n")
 
     obj.vals[kk+1] <- lik.try + evalPen(beta.iter, lambda, gamma, groups)
-    if(kk > 1){
-      if(obj.vals[kk+1] > obj.vals[kk]){
+    if (kk > 1) {
+      if (obj.vals[kk+1] > obj.vals[kk]) {
         beta.iter <- beta.prev
         obj.vals[kk+1] <- obj.vals[kk]
       }
     }
-    # cat(obj.vals[kk+1],"\n")
-    if(kk > 10){
-      if(all(abs((obj.vals[kk:(kk-5)] - obj.vals[(kk+1):(kk-4)])) < tol * abs(obj.vals[1]))){
+    if (kk > 10) {
+      if (all(abs((obj.vals[kk:(kk-5)] - obj.vals[(kk+1):(kk-4)])) < tol * abs(obj.vals[1]))) {
         break
       }
     }
@@ -263,7 +225,7 @@ HierMultNestedOverlap.Path <- function(X, Y, groups, ngamma = 100, delta = 0.01,
   K <- dim(Y)[2]
   x.sd.temp <- apply(X, 2, sd)
   Xtrain <- cbind(1, (X - tcrossprod(rep(1, n), colMeans(X)))/(tcrossprod(rep(1, n), x.sd.temp)))
-  if(any(x.sd.temp==0)){
+  if (any(x.sd.temp==0)) {
     Xtrain[, (which(x.sd.temp==0) + 1)] <- 0
   }
   grad.temp <- crossprod(Xtrain, Y - rep(1, n)%*%t(colMeans(Y))/n)
@@ -275,12 +237,12 @@ HierMultNestedOverlap.Path <- function(X, Y, groups, ngamma = 100, delta = 0.01,
   beta.init0[1,] <- log(margPi) 
   gamma.matrix <- matrix(0, nrow=ngamma, ncol=length(lambda.vec))
   beta.array <- array(0, dim=c(p, K, length(lambda.vec), length(gamma.vec)))
-  for(kk in 1:length(lambda.vec)){
+  for (kk in 1:length(lambda.vec)) {
     beta.init <- beta.init0
-    for(jj in 1:length(gamma.vec)){
+    for (jj in 1:length(gamma.vec)) {
       temp <- AccPGD_HierMultNestedOverlap(Xtrain, Y, groups, beta.init, lambda = lambda.vec[kk], gamma = gamma.vec[jj], max.iter = max.iter, tol = tol)
       #cat(sum(temp[-1,] == 0), "\n")
-      if(sum(temp[-1,] == 0) < length(c(temp[-1,]))){
+      if (sum(temp[-1,] == 0) < length(c(temp[-1,]))) {
         gamma.matrix[1,kk] <- gamma.vec[jj-1]
         gamma.vec <- 10^seq(log10(gamma.matrix[1,kk]), log10(delta*gamma.matrix[1,kk]), length=ngamma)
         break
@@ -288,12 +250,11 @@ HierMultNestedOverlap.Path <- function(X, Y, groups, ngamma = 100, delta = 0.01,
         beta.init <- temp
       }
     }
-    #cat("# --------", kk,"\n")
   }
 
 
 
-  for(kk in 1:length(lambda.vec)){
+  for (kk in 1:length(lambda.vec)) {
     gamma.matrix[,kk] <- 10^seq(log10(gamma.matrix[1,kk]), log10(delta*gamma.matrix[1,kk]), length=ngamma)
   }
 
@@ -305,22 +266,22 @@ HierMultNestedOverlap.Path <- function(X, Y, groups, ngamma = 100, delta = 0.01,
   beta.init0[1,] <- log(margPi) 
   beta.array <- array(0, dim=c(p, K, length(lambda.vec), ngamma))
   val.errs <- matrix(Inf, nrow=length(lambda.vec), ncol=ngamma)
-  for(kk in 1:length(lambda.vec)){
+  for (kk in 1:length(lambda.vec)) {
     beta.init <- beta.init0
-    for(jj in 1:ngamma){
+    for (jj in 1:ngamma) {
       temp <- AccPGD_HierMultNestedOverlap(Xtrain, Y, groups, beta.init, lambda = lambda.vec[kk], gamma = gamma.matrix[jj,kk], max.iter = max.iter, tol = tol)
       beta.array[,,kk,jj] <- temp
       val.errs[kk,jj] <- validationLik(X, Xval, Yval, temp)
-      if(jj > 10 & val.errs[kk,jj] < val.errs[kk,1]){
-        if(all(val.errs[kk,(jj):(jj-4)] > val.errs[kk,(jj-1):(jj-5)])){
+      if (jj > 10 & val.errs[kk,jj] < val.errs[kk,1]) {
+        if (all(val.errs[kk,(jj):(jj-4)] > val.errs[kk,(jj-1):(jj-5)])) {
           break
         }
       }
       cat(val.errs[kk,jj], "\n")
       beta.init <- temp
     }
-    if(kk > 3){
-      if(all(val.errs[kk,] > min(val.errs[kk-1,])) & all(val.errs[kk-1,] > min(val.errs[kk-2,])) & all(val.errs[kk-2,] > min(val.errs[kk-3,]))){
+    if (kk > 3) {
+      if (all(val.errs[kk,] > min(val.errs[kk-1,])) & all(val.errs[kk-1,] > min(val.errs[kk-2,])) & all(val.errs[kk-2,] > min(val.errs[kk-3,]))) {
         break
       }
     }
